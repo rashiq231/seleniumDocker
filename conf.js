ba62f99d3ce7fc25eff7589c17d35dc10bda9587
Rashiq Shaik <mail2.rashiq@gmail.com>
2020-10-06 07:19:38 +0530

    updating selenium address

--- conf.js
+++ conf.js
@@ -1,5 +1,5 @@
 exports.config = {
-  seleniumAddress: "http://172.24.0.2:4444/wd/hub",
+  seleniumAddress: "http://172.24.0.3:4444/wd/hub",
   specs: ["test.js"],
   framework: "jasmine",
   capabilities: {
